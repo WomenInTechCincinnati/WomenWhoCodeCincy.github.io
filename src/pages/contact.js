66af061d3d64d9417c97ed6bab0b7d7f623eb166
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-10-11 15:03:32 -0400

    retrieving wwc contact info at buildtime

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,26 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby";
 
-const ContactPage = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <h1>Contact</h1>
-    
-  </Layout>
-)
+const ContactPage = () => {
+  const contactInfo = useStaticQuery(graphql`
+    query {
+      contact {
+        email
+        links {
+          twitter
+        }
+      }
+    }`)
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <h1>Contact Info</h1>
+      <p>{contactInfo.contact.email}</p>
+      <a href={contactInfo.contact.links.twitter}>twitter</a>
+    </Layout>
+  )
+}
 
 export default ContactPage
