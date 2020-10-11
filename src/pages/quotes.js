19846963ad68c2ea5bcc131409cf55cf1b5b4788
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-10-11 11:40:57 -0400

    added skeleton for members, contact, quotes, and resources pages

---
+++ src/pages/quotes.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const QuotesPage = () => (
+  <Layout>
+    <SEO title="Quotes" />
+    <h1>Quotes</h1>
+    
+  </Layout>
+)
+
+export default QuotesPage
---
+++ src/pages/resources.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ResourcesPage = () => (
+  <Layout>
+    <SEO title="Resources" />
+    <h1>Resources</h1>
+    
+  </Layout>
+)
+
+export default ResourcesPage
