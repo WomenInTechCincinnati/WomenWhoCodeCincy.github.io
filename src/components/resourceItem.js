ce5928183cd945a0c5f9b988fbbf30c3115fa46e
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-10-11 15:39:36 -0400

    fetching resources

---
+++ src/components/resourceItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from "prop-types"
+const ResourceItem = ({ title, description, url, topics }) => (
+  <li>
+    <p>{title}</p>
+    <p>{description}</p>
+    <p>{topics.join(", ")}</p>
+    <a target="_blank" rel="noopener noreferrer" href={url}>{url}</a>
+  </li>
+)
+
+ResourceItem.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  url: PropTypes.string,
+  topics: PropTypes.arrayOf(PropTypes.string)
+}
+
+ResourceItem.defaultProps = {
+  title: ``,
+  description: ``,
+  url: ``,
+  topics: []
+}
+
+
+
+export default ResourceItem
