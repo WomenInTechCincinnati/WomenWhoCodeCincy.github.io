28049a5625979b5a5aff78c67dccfec10aed1f65
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-10-11 11:02:24 -0400

    added women who code in package.json and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Women Who Code Cincinnati`,
+    description: `Women Who Code is a non-profit organization dedicated to inspiring women to excel in technology careers`,
+    author: `@CincyWWCode`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
