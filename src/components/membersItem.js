0d8d90dec7cdb6924d046a454fe4502689554c10
LeeAnne Galioto <10750369+galiotoweb@users.noreply.github.com>
2020-10-31 19:56:14 -0400

    Move membersList to it's own component

---
+++ src/components/membersItem.js
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const MembersItem = ({ name, image, location, techs, hobbies, links }) => (
+  <div>
+    {image && <img src={image} alt={name + `'s beautiful face`} style={{ maxWidth: 200 }}
+      onError={(event) => { event.target.onerror = null; event.target.src = "https://avatars3.githubusercontent.com/u/13358086?s=200&v=4" }}
+    />}
+    <h2>{name}</h2>
+    {location && <p>{location}</p>}
+    {techs && <div><h3>Favorite Technologies</h3><ul>{techs.map((tech, index) => <li key={index}>{tech}</li>)}</ul></div>}
+    {hobbies && <div><h3>Hobbies</h3><ul>{hobbies.map((hobby, index) => <li key={index}>{hobby}</li>)}</ul></div>}
+    {links && <div><h3>Contact Info</h3>
+      <ul>
+        {links.twitter && <li><a href={links.twitter} >Twitter</a> </li>}
+        {links.facebook && <li><a href={links.facebook} >Facebook</a> </li>}
+        {links.linkedIn && <li><a href={links.linkedIn} >LinkedIn</a> </li>}
+        {links.website && <li><a href={links.website} >Website</a> </li>}
+        {links.github && <li><a href={links.github} >Github</a> </li>}
+        {links.meetup && <li><a href={links.meetup} >Meetup</a> </li>}
+      </ul>
+    </div>}
+    <hr />
+  </div>
+)
+
+MembersItem.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  location: PropTypes.string,
+  techs: PropTypes.arrayOf(PropTypes.string),
+  hobbies: PropTypes.arrayOf(PropTypes.string),
+  links: PropTypes.shape({
+    twitter: PropTypes.string,
+    facebook: PropTypes.string,
+    linkedIn: PropTypes.string,
+    website: PropTypes.string,
+    github: PropTypes.string,
+    meetup: PropTypes.string
+  })
+}
+
+MembersItem.defaultProps = {
+  name: ``,
+  image: ``,
+  location: ``,
+  techs: [],
+  hobbies: [],
+  links: {
+    twitter: ``,
+    facebook: ``,
+    linkedIn: ``,
+    website: ``,
+    github: ``,
+    meetup: ``
+  }
+}
+
+export default MembersItem
--- src/pages/members.js
+++ src/pages/members.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React, { Component } from "react";
+import Layout from "../components/layout";
+import MembersItem from "../components/MembersItem";
+import SEO from "../components/seo";
 import { fetchResults } from "../utils/api";
 
 class MembersPage extends Component {
   state = {
-    fetchedMembersList: [],
+    membersList: [],
     hasError: false
   }
 
@@ -13,7 +14,7 @@ class MembersPage extends Component {
     try {
       const response = await fetchResults("http://my-json-server.typicode.com/WomenWhoCodeCincy/wwcode-cincy-database/members");
       if (response.success) {
-        this.setState({ fetchedMembersList: response.results, hasError: false });
+        this.setState({ membersList: response.results, hasError: false });
       } else {
         this.setState({ hasError: true });
       }
@@ -22,42 +23,25 @@ class MembersPage extends Component {
     }
   }
 
-  // Backup Image in case url from database is broken
-  addDefaultSrc(ev) {
-    ev.target.src = "https://avatars3.githubusercontent.com/u/13358086?s=200&v=4"
-  }
-
-
   render() {
-    const { fetchedMembersList, hasError } = this.state
-
-    const membersList = fetchedMembersList.map((member, index) => <div key={member.name + index}>
-
-      {member.image_url && <img onError={this.addDefaultSrc} src={member.image_url} alt={member.name + `'s beautiful face`} style={{ maxWidth: 200 }} />}
-      <h2>{member.name}</h2>
-      {member.location && <div><p>{member.location}</p></div>}
-      {member.favorite_techs && <div><h3>Favorite Technologies</h3><ul>{member.favorite_techs.map((tech, index) => <li key={index}>{tech}</li>)}</ul></div>}
-      {member.hobbies && <div><h3>Hobbies</h3><ul>{member.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>)}</ul></div>}
-      {member.links && <div><h3>Contact Info</h3>
-        <ul>
-          {member.links.twitter && <li><a href={member.links.twitter} >Twitter</a> </li>}
-          {member.links.facebook && <li><a href={member.links.facebook} >Facebook</a> </li>}
-          {member.links.linkedIn && <li><a href={member.links.linkedIn} >LinkedIn</a> </li>}
-          {member.links.website && <li><a href={member.links.website} >Website</a> </li>}
-          {member.links.github && <li><a href={member.links.github} >Github</a> </li>}
-          {member.links.meetup && <li><a href={member.links.meetup} >Meetup</a> </li>}
-        </ul>
-      </div>}
-      <hr />
-    </div>)
-
+    const { membersList, hasError } = this.state
 
     return (<Layout>
       <SEO title="Chapter Members" />
       <h1>Chapter Members</h1>
       {hasError && <p>Unable to fetch members.</p>}
       <section className="members">
-        {membersList}
+        {membersList.map((member, index) => (
+          <MembersItem
+            key={index}
+            name={member.name}
+            image={member.image_url}
+            location={member.location}
+            techs={member.favorite_techs}
+            hobbies={member.hobbies}
+            links={member.links}
+          />
+        ))}
       </section>
     </Layout >)
   }
